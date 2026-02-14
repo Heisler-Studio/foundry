0d7229823136fdaeadbcc73ba79f9921f6bd64dd
Evan Heisler <evanheisler@gmail.com>
2026-02-14 11:22:45 -0800

    cleanup home layout, notes

--- apps/base-universal-app/components/screens/home/index.tsx
+++ apps/base-universal-app/components/screens/home/index.tsx
@@ -1,10 +1,15 @@
 import StyledExample from '@/components/example/StyledExample';
+import { Text } from '@/components/primitives/ThemedText';
+import { useHeaderHeight } from '@react-navigation/elements';
 import { KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
 
 export default function Home() {
+  const headerHeight = useHeaderHeight();
+
   return (
     // NOTE: SafeAreaView will trigger opaque StatusBar area using the page backgroundColor
     <KeyboardAvoidingView
+      style={{ flex: 1 }}
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       // Offset is usually the height of your header if using one
       keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 20}
@@ -13,10 +18,24 @@ export default function Home() {
         keyboardShouldPersistTaps="handled"
         // Ensures that content starts below the header/status bar initially but allows it to scroll behind it with the blur effect
         contentInsetAdjustmentBehavior="automatic"
-        contentContainerClassName="sm:flex-1 items-center justify-center p-4 py-8 sm:py-4 sm:p-6 mt-safe"
+        // Web/Android: Manual compensation for the transparent header
+        contentContainerStyle={{
+          paddingTop: Platform.OS !== 'ios' ? headerHeight : 0,
+        }}
+        contentContainerClassName="p-4 py-8"
         keyboardDismissMode="interactive"
       >
-        <View className="w-full gap-4">
+        <View className="w-full">
+          <Text>* Hero: Heisler Studio</Text>
+          <View className="p-1" />
+          <Text>
+            * Horizontal list of cards for work history: Thrive, Fountain Life, Bionic Health,
+            LifeOmic
+          </Text>
+          <View className="p-1" />
+          <Text>* Tab bar: About, contact</Text>
+          <View className="p-1" />
+          <Text>* Menu: GitHub, LinkedIn</Text>
           <StyledExample />
         </View>
       </ScrollView>
